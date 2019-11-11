d6e33ff850a3fb91886dd8fceb94a2d22b788709
ChloeJ <chloe.jandsten@gmail.com>
2019-11-11 12:17:30 -0500

    seperated into 2 files:

--- bash.js
+++ bash.js
@@ -1,11 +1,3 @@
+const pwd = require('./pwd')
 process.stdout.write('prompt >');
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
-    if (cmd === 'pwd') {
-        process.stdout.write(`${process.cwd()}`);
-        process.stdout.write('\nprompt > ');
-    }
 
- //   process.stdout.write('You typed: ' + cmd);
- //   process.stdout.write('\nprompt > ');
-});
---
+++ pwd.js
@@ -0,0 +1,20 @@
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd') {
+        process.stdout.write(`${process.cwd()}`);
+        process.stdout.write('\nprompt > ');
+    }
+
+});
+
+module.exports = function(){
+    process.stdin.on('data', (data) => {
+        const cmd = data.toString().trim();
+        if (cmd === 'pwd') {
+            process.stdout.write(`${process.cwd()}`);
+            process.stdout.write('\nprompt > ');
+        }
+    });
+}
+
